8a1de066dbb3ec7020f5ceec605a7356ec2621bf
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2022-10-19 13:16:32 +0530

    primenumberornot

---
+++ primenumberornot.js
@@ -0,0 +1,39 @@
+check tehe number is prime or notify
+
+let num=13;
+  let count=0;
+    for(let i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==!2)
+    {
+        console.log("Yes");
+    }
+    else 
+    {
+        console.log("No");
+    }
+	
+	
+	 let bag="";
+    for(let i=str.length-1;i>=0;i--)
+    {
+        bag+=str[i];
+    }
+    // console.log(bag);
+    if(bag==!str)
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
+	check weather the string ids palindrome or not
+	
+	let str="Nurpul";
+	
